cfa85ef5ac6003052b17a31e090ccbf97fcd9fdf
Shivang Kushwaha <shivangkushwaha025@gmail.com>
2024-02-16 12:22:05 +0530

    Added new enviorment

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\coding_data\python\fast-api\venv
+set VIRTUAL_ENV=D:\coding_data\fast-api-learning\fast-api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
